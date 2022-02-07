d75a1e019a20f7281b8dd56421fd71e7e65a9668
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-02-07 14:27:34 +0000

    done on 7.feb.2022

--- p3original.c
+++ p3original.c
@@ -1,40 +1,36 @@
 #include<stdio.h>
-int input_number();
-int is_composite(int n);
-void output(int n, int composite);
-
-int main()
-{
- int n,composite;
- input_number();
- is_composite(n);
- output(n,composite);
- return 0;
-}
 int input_number()
 {
- int n;
- printf("enter a number\n");
- scanf("%d",&n);
- return n;
+    int n;
+    printf("ENTER YOUR NUMBER\n");
+    scanf("%d",&n);
+    return n;
 }
-
 int is_composite(int n)
 {
-    int composite;
-    if(n%n==1 && n%1==n)
+    int i,composite;
+    for(i=1;i<n/2;i++)
     {
-      composite=1;
+        if(n%i==0)
+        {
+         composite=1;
+        }
     }
     return composite;
 }
+ void output(int n,int composite)
+ {
+     if(composite==1)
+     printf("%d IS COMPOSITE NUMBER",n);
+     else 
+     printf("%d IS NOT COMPOSITE NUMBER",n);
 
-void output(int n,int composite)
-{
-    if(composite==1)
-    printf("%d is composite \n",n);
-   else
-    printf("%d is not composite",n);
-} 
-
-
+ }
+ int main()
+ {
+ int n,composite;
+ n=input_number();
+ composite=is_composite(n);
+ output(n,composite);
+ return 0;
+ }
